5bf4dadd88cc6e0c205c7577facbb6734a8cfb21
Roman Fuentes <romangof@gmail.com>
2017-08-17 13:01:24 -0300

    Added %PUBLIC_URL% to header links

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default () => <ul className="nav navbar-nav navbar-right">
     <li><a href="como-usar.html">cómo usar</a></li>
-    <li><a style={{color: 'white'}} href="para-quien4.html">para quién es</a></li>
-    <li><a style={{color: 'white'}} href="mi-ciudad.html">mi ciudad</a></li>
-    <li><a style={{color: 'white'}} href="inversionistas.html">Inversionistas</a></li>
-    <li><a style={{color: 'white'}} href="contacto.html">Contacto</a></li>
+    <li><a style={{color: 'white'}} href="%PUBLIC_URL%/para-quien4.html">para quién es</a></li>
+    <li><a style={{color: 'white'}} href="%PUBLIC_URL%/mi-ciudad.html">mi ciudad</a></li>
+    <li><a style={{color: 'white'}} href="%PUBLIC_URL%/inversionistas.html">Inversionistas</a></li>
+    <li><a style={{color: 'white'}} href="%PUBLIC_URL%/contacto.html">Contacto</a></li>
   </ul>
